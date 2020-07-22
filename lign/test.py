abfe9c370150524db5ed5c5872a033610d0b8910
JosueCom-winter <josue.n.rivera@outlook.com>
2020-07-22 18:43:40 -0400

    Added missing filter to validate graph in test

--- lign/test.py
+++ lign/test.py
@@ -4,15 +4,18 @@ import torch as th
 from .utils import io
 
 def metrics(model, graph, train, tag_in, tag_out, vec_size, labels, metrics = ['accuracy'], cluster = (cl.NN(), 3), device = (th.device('cpu'), None)):
-    tr_nodes, tr_labs = cl.filter_k(tag_out, train, labels, cluster[1])
+    tr_nodes, tr_labs = cl.filter_k(tag_out, labels, train, cluster[1])
     sub = train.subgraph(tr_nodes)
     inp = sub.get_parent_data(tag_in)
     
     cluster = cluster[0]
     cluster.train(model(sub, inp), tr_labs)
 
-    inp = graph.get_data(tag_in).to(device(0))
-    outp_t = graph.get_data(tag_out).to(device(0))
+    ts_nodes = cl.filter(tag_out, labels, graph)
+    graph = graph.subgraph(ts_nodes)
+
+    inp = graph.get_parent_data(tag_in).to(device(0))
+    outp_t = graph.get_parent_data(tag_out).to(device(0))
 
     rep_vec = model(graph, inp)
     outp_p = cluster(rep_vec)
