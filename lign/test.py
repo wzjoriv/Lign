ff52441ef512c0920f4de86e7172e7f9234001d1
Josue N Rivera <josue.n.rivera@outlook.com>
2021-06-25 22:23:03 -0400

    Initiated work on 2 validation type
    -> vector + graph data

--- lign/test.py
+++ lign/test.py
@@ -5,6 +5,87 @@ from lign.utils import io
 import matplotlib.pyplot as plt
 import numpy as np
 
+def g_validate(model, graph, train, tag_in, tag_out, labels, metrics = ['accuracy'], sv_img = None, cluster = (cl.NN(), 3), device = th.device('cpu'), save_img = False):
+
+    model.eval()
+    with th.no_grad():
+        tr_nodes, tr_labs = cl.filter_k(tag_out, labels, train, cluster[1])
+        sub = train.subgraph(tr_nodes)
+        inp = sub.get_parent_data(tag_in).to(device)
+        
+        cluster = cluster[0]
+        cluster.train(model(sub, inp), tr_labs.to(device))
+
+        ts_nodes = cl.filter(tag_out, labels, graph)
+        graph = graph.subgraph(ts_nodes)
+
+        inp = graph.get_parent_data(tag_in).to(device)
+        outp_t = graph.get_parent_data(tag_out).to(device)
+
+        rep_vec = model(graph, inp, save=save_img)
+        outp_p = cluster(rep_vec)
+
+
+    ## save 2d image
+    if sv_img is not None:
+        if sv_img == '2d':
+
+            fig = plt.figure()
+
+            tp = rep_vec.cpu().detach().numpy()
+            tp2 = outp_t.cpu().detach().numpy()
+            c = plt.scatter(tp[:, 0], tp[:, 1], c=tp2, cmap=plt.get_cmap('gist_rainbow'))
+            #plt.ylim([-1.2,1.2])
+            #plt.xlim([-1.2,1.2])
+
+            plt.colorbar(c).set_label("Label")
+            plt.savefig("data/views-2d/Validate "+str(len(labels))+".png")
+            plt.close()
+
+
+        elif sv_img == '3d':
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+
+            tp = rep_vec.cpu().detach().numpy()
+            tp2 = outp_t.cpu().detach().numpy()
+            c = ax.scatter(tp[:, 2], tp[:, 3], tp[:, 4], c=tp2, cmap=plt.get_cmap('gist_rainbow'))
+
+            plt.colorbar(c).set_label("Label")
+            plt.savefig("data/views-3d/Validate "+str(len(labels))+".png")
+            plt.close()
+
+
+        elif sv_img == 'bt':
+            subset = 20
+
+            inp_tp = inp.cpu().detach().numpy()
+            tp = rep_vec.cpu().detach().numpy()
+            tp2 = outp_t.cpu().detach().numpy()
+            tp3 = outp_p.cpu().detach().numpy()
+
+            para = {
+                "PREDICTION": tp3[:subset].tolist(),
+                "TRUTH": tp2[:subset].tolist(),
+                "VECTOR": tp[:subset].tolist(),
+            }
+
+            io.json(para, "data/views-bt/Validate "+str(len(labels))+".json")
+            
+            for i in range(subset):
+                plt.imshow(inp_tp[i][0], interpolation='nearest')
+                plt.savefig("data/views-bt/imgs/Validate "+str(len(labels))+ "-" + str(i) + ".png")
+
+            plt.close()
+
+    out = {}
+    metrics = io.to_iter(metrics)
+    for metric in metrics:
+        if metric == 'accuracy':
+            out[metric] = (outp_p == outp_t).sum().item() * 100.0/outp_p.size(0)
+    
+    return out
+
 def validate(model, graph, train, tag_in, tag_out, labels, metrics = ['accuracy'], sv_img = None, cluster = (cl.NN(), 3), device = th.device('cpu'), save_img = False):
 
     model.eval()
