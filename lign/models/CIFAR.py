b7a3ce83a083cf91fe50ffd0710dc8c687cf8a9c
Josue N Rivera <josue.n.rivera@outlook.com>
2021-07-03 13:51:15 -0400

    CIFAR changed to say unit instead of gcn

--- lign/models/CIFAR.py
+++ lign/models/CIFAR.py
@@ -7,21 +7,21 @@ from lign.layers import DyLinear
 class Base(nn.Module):  ## base, feature extractor
     def __init__(self, out_feats):
         super(Base, self).__init__()
-        self.gcn1 = nn.Conv2d(3, 6, 5)
-        self.gcn2 = nn.Conv2d(6, 16, 5)
-        self.gcn3 = nn.Linear(16 * 5 * 5, 150)
-        self.gcn4 = nn.Linear(150, 84)
-        self.gcn5 = nn.Linear(84, out_feats)
+        self.unit1 = nn.Conv2d(3, 6, 5)
+        self.unit2 = nn.Conv2d(6, 16, 5)
+        self.unit3 = nn.Linear(16 * 5 * 5, 150)
+        self.unit4 = nn.Linear(150, 84)
+        self.unit5 = nn.Linear(84, out_feats)
         self.pool = nn.MaxPool2d(2, 2)
 
     def forward(self, g, features, save=False):
-        x = self.pool(g, F.relu(self.gcn1(g, features)))
-        x = self.pool(g, F.relu(self.gcn2(g, x)))
+        x = self.pool(g, F.relu(self.unit1(g, features)))
+        x = self.pool(g, F.relu(self.unit2(g, x)))
         x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.gcn3(g, x))
-        x = F.relu(self.gcn4(g, x))
+        x = F.relu(self.unit3(g, x))
+        x = F.relu(self.unit4(g, x))
         
-        return th.tanh(self.gcn5(g, x))
+        return th.tanh(self.unit5(g, x))
 
 
 class Classifier(nn.Module): ## temporality layer for training
