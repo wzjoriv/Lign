fc9b594fb2bfca466bd4334bc976d147b5417097
Josue N Rivera <josue.n.rivera@outlook.com>
2021-07-11 16:28:11 -0400

    Set python requirement >=3.7

--- lign/utils/types.py
+++ lign/utils/types.py
@@ -1,19 +1,25 @@
-
+from __future__ import annotations
 from torch.functional import Tensor
+from collections import abc
+import sys
 
-import torch as th
+import typing
+from typing import TypeVar
 
 try:
-    from typing import TypeVar, Dict, List, TypedDict
+    from typing import TypedDict
 except Exception:
-    from typing import TypeVar, Dict, List
     from typing_extensions import TypedDict
 
+if sys.version > (3, 9):
+    Callable = abc.Callable
+    Dict = abc.dict
+    List = abc.list
+    Tuple = abc.tuple
+else:
+    Callable = typing.Callable
+    Dict = typing.Dict
+    List = typing.List
+    Tuple = typing.Tuple
+
 T = TypeVar('T')
-
-Node = TypedDict('Node', {
-    'data': Dict[str, T], 
-    'edges': List[int]
-    })
-
-Tensor = th.Tensor
