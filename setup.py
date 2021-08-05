35b651d1c86b0a727f5de47b8d01181e112224d6
Josue N Rivera <josue.n.rivera@outlook.com>
2021-08-05 01:12:55 -0400

    Mini description updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r") as f:
 
 setup(name='lign',
 		version='0.1.1',
-		description='A framework for working with graphs alongside PyTorch',
+		description='A graph framework that works alongside PyTorch',
 		long_description=long_description,
 		long_description_content_type="text/markdown",
 		license="Mozilla Public License Version 2.0",
